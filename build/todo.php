c63d973127f0d42649c6b48d7df097f7504f4a25
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-23 21:59:29 +0200

    e

--- build/todo.php
+++ build/todo.php
@@ -4,8 +4,8 @@
 // TODO: auth - (verify) register
 
 // TODO: log everything (ster opdracht - new,edit,delete,join |  auth - login,logout,password change/reset,CSRF,session expired, ip session stolen, user tried to access unauthoprized page (leerlingen,docenten,admin) | sql - querys (maybe))
-
 // TODO: security - check all forms for csrf
+
 // TODO: agenda - improve ui (probaly wont do it)
 
 // TODO: steropdracht - search based on id - https://stackoverflow.com/questions/43480162/hide-all-elements-except-one-and-show-all-elements-again/43480468 - https://stackoverflow.com/questions/13239331/hide-all-divs-except-one
