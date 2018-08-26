f755338f9d4bff0f98f3d7473f5629ddadda2f07
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-26 19:35:07 +0200

    update

--- build/todo.php
+++ build/todo.php
@@ -1,7 +1,5 @@
 <?php
 
-// TODO: auto backup db
-
 // TODO: auth - register system
 // TODO: security - check all forms for csrf
 
@@ -11,10 +9,10 @@
 
 // TODO: steropdracht - search based on id - https://stackoverflow.com/questions/43480162/hide-all-elements-except-one-and-show-all-elements-again/43480468 - https://stackoverflow.com/questions/13239331/hide-all-divs-except-one
 
-// TODO: security - CSP policy - https://scotthelme.co.uk/content-security-policy-an-introduction/
-
 // TODO: transfer db to dell server
 
 // TODO: agenda - add item system
 // TODO: agenda - support for pdf
 // TODO: agenda - improve ui
+
+// TODO: security - CSP policy - https://scotthelme.co.uk/content-security-policy-an-introduction/
