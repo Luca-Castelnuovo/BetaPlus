3b07acd73b3a9b1c5d5f740614fc97393175ecd3
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-31 18:49:33 +0200

    changed gen.js [deploy:Production]

--- admin/index.php
+++ admin/index.php
@@ -58,4 +58,4 @@ head('Admin', 5);
         </div>
     </div>
 </div>
-<?php footer("<script>var instance = M.Tabs.init(document.querySelector('.tabs'), {});</script><script src='https://cdn.lucacastelnuovo.nl/js/betasterren/gen.2.js'></script>"); ?>
+<?php footer("<script>var instance = M.Tabs.init(document.querySelector('.tabs'), {});</script><script src='https://cdn.lucacastelnuovo.nl/js/betasterren/gen.3.js'></script>"); ?>
