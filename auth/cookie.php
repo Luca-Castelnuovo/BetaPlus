20304904d077556f3f4c5db77051ac42016771d1
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-08 12:52:52 +0200

    removed debug [deploy:Production]

--- auth/cookie.php
+++ auth/cookie.php
@@ -3,9 +3,7 @@
 require($_SERVER['DOCUMENT_ROOT'] . "/init.php");
 
 if (!isset($_COOKIE['REMEMBERME'])) {
-    echo '2';
-    exit;
-    // redirect('/?logout');
+    redirect('/?logout');
 }
 
 list($user, $leerling, $token, $mac) = explode(':', $_COOKIE['REMEMBERME']);
@@ -31,14 +29,10 @@ $token_sql = sql_query($query, true);
 $config = config_load();
 
 if (!hash_equals(hash_hmac('sha512', $user . ':' . $leerling . ':'. $token, $config['hmac_key']), $mac)) {
-    echo '3';
-    exit;
-    // redirect('/?logout');
+    redirect('/?logout');
 }
 if (!hash_equals($token_sql['token'], $token)) {
-    echo '4';
-    exit;
-    // redirect('/?logout');
+    redirect('/?logout');
 }
 
 $table = ($leerling == 1) ? 'leerlingen' : 'docenten';
@@ -61,16 +55,12 @@ $user = sql_query($query, true);
 
 if ($user['failed_login'] > 4) {
     log_action($user['first_name'] . ' ' . $user['last_name'], 'Too many failed login attempts', 2);
-    echo '5';
-    exit;
-    // redirect('/?reset', 'Uw account is geblokkeerd door teveel mislukt inlogpogingen, contacteer AUB de administrator');
+    redirect('/?reset', 'Uw account is geblokkeerd door teveel mislukt inlogpogingen, contacteer AUB de administrator');
 }
 
 if (!$user['active']) {
     log_action($user['first_name'] . ' ' . $user['last_name'], 'Account Inactive', 2);
-    echo '6';
-    exit;
-    // redirect('/?reset', 'Uw account is niet actief, contacteer AUB de administrator');
+    redirect('/?reset', 'Uw account is niet actief, contacteer AUB de administrator');
 }
 
 sql_query("UPDATE leerlingen SET failed_login='0' WHERE id='{$user['id']}' AND class='{$user['class']}'", false);
@@ -97,11 +87,7 @@ if (isset($user['admin'])) {
 session_regenerate_id(true);
 
 if (!empty($return_url)) {
-    echo '7';
-    exit;
-// redirect($return_url, 'U bent ingelogd');
+    redirect($return_url, 'U bent ingelogd');
 } else {
-    echo '8';
-    exit;
-    // redirect('/general/home', 'U bent ingelogd');
+    redirect('/general/home', 'U bent ingelogd');
 }
