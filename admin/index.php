6adec47cba37df22a581e0da4ea0860609d8e745
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-10 18:13:19 +0200

    added are you sure btn [deploy:Production]

--- admin/index.php
+++ admin/index.php
@@ -60,7 +60,7 @@ $_SESSION['toast_set'] = true;
                 </div>
                 <div class="row">
                     <h3>Sessions</h3>
-                    <a href="/admin/process/<?= csrf_gen() ?>/remember///" class="waves-effect waves-light btn-large color-secondary--background"><i class="material-icons left">code</i>Destroy all sessions</a>
+                    <a href="/admin/process/<?= csrf_gen() ?>/remember///" class="waves-effect waves-light btn-large color-secondary--background" onclick="return confirm('Weet je het zeker?')"><i class="material-icons left">code</i>Destroy all sessions</a>
                 </div>
             </div>
         </div>
