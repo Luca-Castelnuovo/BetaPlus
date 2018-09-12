0d50fc6dfcab5bb94f332788e4ec5516bfb9c45b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-12 18:18:33 +0200

    removed register validation tmp for bulk registration [deploy:Production]

--- build/todo.php
+++ build/todo.php
@@ -1,6 +1,6 @@
 <?php
 
-// TODO: steropdrachten - file system
+// TODO: steropdrachten - add/delete file system
 
 // TODO: filter in admin panel for logs - https://www.w3schools.com/howto/howto_js_filter_table.asp - https://stackoverflow.com/questions/51187477/how-to-filter-a-html-table-using-simple-javascript - https://codepen.io/chriscoyier/pen/tIuBL
 // (SEARCH FIELD) - low  / medium / high
