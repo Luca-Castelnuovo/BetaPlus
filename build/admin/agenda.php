76e18419455b323db5756d06c16d1f389fdc0feb
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-21 23:01:07 +0200

    tmp version of agenda view ui

--- build/admin/agenda.php
+++ build/admin/agenda.php
@@ -1,1 +1,21 @@
 <?php
+require($_SERVER['DOCUMENT_ROOT'] . '/init.php');
+
+login_docent();
+
+head('Agenda Panel', 5);
+
+?>
+
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h3>Dit moet nog gedaan worden</h3>
+                <?php // TODO: agenda input form?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php footer(); ?>
--- build/includes/generic.php
+++ build/includes/generic.php
@@ -56,3 +56,38 @@ function alert_display()
         unset($_SESSION['alert']);
     }
 }
+
+function agenda()
+{
+    $query =
+        "SELECT
+            title,
+            link,
+            date
+        FROM
+            agenda
+        WHERE
+            DATE(date) >= DATE(NOW())";
+
+    $items = sql_query($query, false);
+
+    if ($items->num_rows > 0) {
+        while ($item = $items->fetch_assoc()) {
+            echo <<<END
+            <div class="col s12 m6 l4 xl4">
+                <div class="card small center">
+                    <div class="card-content white-text">
+                      <span class="card-title">{$item['title']}</span>
+                    </div>
+                    <div class="card-action">
+                        <a href="javascript:void(0)" target="_blank">{$item['date']}</a>
+                        <a href="{$item['link']}" target="_blank">Link</a>
+                    </div>
+                </div>
+            </div>
+END;
+        }
+    } else {
+        echo '<p>Er zijn op dit moment geen agenda items.</p> ';
+    }
+}
