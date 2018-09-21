9152ed5ec3c313cfcd07b04f3abb5f14f4b63f58
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-21 18:47:47 +0200

    forgot to remove ! to reverse statement

--- ster-opdrachten/files.php
+++ ster-opdrachten/files.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
 
-    if (!empty($_FILES['file'])) {
+    if (empty($_FILES['file'])) {
         redirect('/ster-opdrachten/files/' . $id);
     }
 
