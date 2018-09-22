c0509097246dafdfbae6502f8f50952790afa694
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-09-22 14:36:00 +0200

    moved js to cdn

--- admin/index.php
+++ admin/index.php
@@ -34,41 +34,6 @@ head('Admin', 5);
             </div>
             <div id="logs" class="col s12">
                 <?php admin_log_list(); ?>
-                <script>(function(document) {
-    'use strict';
-    var LightTableFilter = (function(Arr) {
-        var _input;
-
-        function _onInputEvent(e) {
-            _input = e.target;
-            var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-            Arr.forEach.call(tables, function(table) {
-                Arr.forEach.call(table.tBodies, function(tbody) {
-                    Arr.forEach.call(tbody.rows, _filter);
-                });
-            });
-        }
-
-        function _filter(row) {
-            var text = row.textContent.toLowerCase(),
-                val = _input.value.toLowerCase();
-            row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-        }
-        return {
-            init: function() {
-                var inputs = document.getElementsByClassName('light-table-filter');
-                Arr.forEach.call(inputs, function(input) {
-                    input.oninput = _onInputEvent;
-                });
-            }
-        };
-    })(Array.prototype);
-    document.addEventListener('readystatechange', function() {
-        if (document.readyState === 'complete') {
-            LightTableFilter.init();
-        }
-    });
-})(document);</script>
             </div>
             <div id="extra" class="col s12">
                 <div class="row">
@@ -96,4 +61,4 @@ head('Admin', 5);
         </div>
     </div>
 </div>
-<?php footer("<script>var instance = M.Tabs.init(document.querySelector('.tabs'), {});</script><script src='https://cdn.lucacastelnuovo.nl/js/ajax.js'></script><script src='https://cdn.lucacastelnuovo.nl/js/betasterren/gen.6.js'></script>"); ?>
+<?php footer("<script>var instance = M.Tabs.init(document.querySelector('.tabs'), {});</script><script src='https://cdn.lucacastelnuovo.nl/js/ajax.js'></script><script src='https://cdn.lucacastelnuovo.nl/js/betasterren/gen.6.js'></script><script src='https://cdn.lucacastelnuovo.nl/js/betasterren/filter.js'></script>"); ?>
