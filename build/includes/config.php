53947f142a28c5691d5c1cd81d55f70fdc043dac
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-11-06 11:41:18 +0100

    tmp2

--- build/includes/config.php
+++ build/includes/config.php
@@ -10,15 +10,15 @@ return (object) array(
     ),
 
     'security' => array(
-        'hmac' => array(
-            'key'=>"XpkTQwW6K5Ni4rf3xpF2gNkmT0zlGJhWDtIXQRbWu3CJ5PJznZy4HzBYhJu0z8h",
-        ),
-
         'database' => array(
             'host' => "localhost",
             'user' => "betasterren",
             'password' => "I0$pge69v2ukoHF9iec1YDuli73racMWXC2JM^&$nQkSsy#M",
             'database' => "betasterren_db",
+        ),
+
+        'hmac' => array(
+            'key'=>"XpkTQwW6K5Ni4rf3xpF2gNkmT0zlGJhWDtIXQRbWu3CJ5PJznZy4HzBYhJu0z8h",
         ),
     ),
 
@@ -48,6 +48,7 @@ return (object) array(
 
             'main' => "https://cdn.lucacastelnuovo.nl/css/betasterren/style.css",
             'simplemde' => "https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css",
+            'imgur' => "https://cdn.lucacastelnuovo.nl/css/betasterren/imgur.css",
         ),
 
         'js' => array(
@@ -67,9 +68,23 @@ return (object) array(
             ),
 
             'ajax' => "https://cdn.lucacastelnuovo.nl/js/ajax.js",
+            'gen' => "https://cdn.lucacastelnuovo.nl/js/betasterren/gen.6.js",
+            'filter' => "https://cdn.lucacastelnuovo.nl/js/betasterren/filter.2.js",
+            'imgur' => "https://cdn.lucacastelnuovo.nl/js/betasterren/imgur.php.4.js?response_url=/general/upload.php&type=",
         ),
 
         'images' => array(
+            'icons' => array(
+                '72x72' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-72x72.png",
+                '96x96' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-96x96.png",
+                '128x128' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-128x128.png",
+                '144x144' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-144x144.png",
+                '152x152' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-152x152.png",
+                '192x192' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-192x192.png",
+                '384x384' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-384x384.png",
+                '512x512' => "https://cdn.lucacastelnuovo.nl/images/betasterren/icons/icon-512x512.png",
+            ),
+
             'logo' => "https://cdn.lucacastelnuovo.nl/images/betasterren/logo.png",
             'default_profile' => "https://cdn.lucacastelnuovo.nl/images/betasterren/default_profile.png",
             'hack_attempt' => "https://cdn.lucacastelnuovo.nl/images/dont-hack.png",
