e65ed9fe0b6bd251738d5adeb83cdfa20324af01
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-11-06 21:45:05 +0100

    fixed a data type bug

--- includes/config.php
+++ includes/config.php
@@ -1,46 +1,46 @@
 <?php
 
 return (object) array(
-    'app' => array(
+    'app' => (object) array(
         'url' => "https://betasterren.hetbaarnschlyceum.nl",
         'domain' => "betasterren.hetbaarnschlyceum.nl",
 
-        'admin' => array(
+        'admin' => (object) array(
             'email' => "lucacastelnuovo@hetbaarnschlyceum.nl",
         ),
     ),
 
-    'security' => array(
-        'database' => array(
+    'security' => (object) array(
+        'database' => (object) array(
             'host' => "localhost",
             'user' => "betasterren",
-            'password' => "I0$pge69v2ukoHF9iec1YDuli73racMWXC2JM^&$nQkSsy#M",
+            'password' => 'I0$pge69v2ukoHF9iec1YDuli73racMWXC2JM^&$nQkSsy#M',
             'database' => "betasterren_db",
         ),
 
         'hmac'=>"XpkTQwW6K5Ni4rf3xpF2gNkmT0zlGJhWDtIXQRbWu3CJ5PJznZy4HzBYhJu0z8h",
     ),
 
-    'api' => array(
-        'mail' => array(
+    'api' => (object) array(
+        'mail' => (object) array(
             'key' => "rqc4o57337jp9d9ilueflk6rwl5s48ra",
             'url' => "https://api.lucacastelnuovo.nl/mail/",
         ),
 
-        'recaptcha' => array(
+        'recaptcha' => (object) array(
             'key' => "ln8k76qgxhbtukg2qu8lbxwsc6lgcveay",
             'url' => "https://api.lucacastelnuovo.nl/recaptcha/",
             'library' => "https://www.google.com/recaptcha/api.js",
         ),
 
-        'imgur' => array(
+        'imgur' => (object) array(
             'key' => "b2c72661027878c",
             'url' => "https://cdn.lucacastelnuovo.nl/js/betasterren/imgur.php.4.js",
         ),
     ),
 
-    'cdn' => array(
-        'css' => array(
+    'cdn' => (object) array(
+        'css' => (object) array(
             'materialize' => array(
                 'library' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/css/materialize.css",
                 'icons' => "https://fonts.googleapis.com/icon?family=Material+Icons",
@@ -51,13 +51,13 @@ return (object) array(
             'imgur' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/css/imgur.css",
         ),
 
-        'js' => array(
+        'js' => (object) array(
             'materialize' => array(
                 'library' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/js/materialize.js",
                 'init' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/hbl/js/materialize_init.js",
             ),
 
-            'particle' => array(
+            'particle' => (object) array(
                 'library' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/js/particles.js",
                 'init' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/hbl/js/particles_init.js",
             ),
@@ -68,8 +68,8 @@ return (object) array(
             'filter' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/js/filter.js",
         ),
 
-        'images' => array(
-            'icons' => array(
+        'images' => (object) array(
+            'icons' => (object) array(
                 '72x72' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/hbl/images/icons/icon-72x72.png",
                 '96x96' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/hbl/images/icons/icon-96x96.png",
                 '128x128' => "https://betaplus.ams3.cdn.digitaloceanspaces.com/hbl/images/icons/icon-128x128.png",
